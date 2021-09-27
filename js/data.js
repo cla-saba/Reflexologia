f33d057fedfc8200cdf505dbc7773bcf0aefefb0
cla-saba <87765365+cla-saba@users.noreply.github.com>
2021-09-27 12:54:07 -0300

    Initial commit

---
+++ js/data.js
@@ -0,0 +1,35 @@
+let explicacion =
+  `<div class='texto' id='texto1'> 
+<p>La <strong>reflexología podal </strong>es una técnica concreta de la reflexología, de la que ya hablamos en otro post y que se basa en la idea de que todos los órganos de nuestro cuerpo se reflejan otra parte de cuerpo, en este caso en los pies (de ahí el término de <em>podal)</em>, como si fuera un mapa perfecto. Lo vemos a continuación, en la siguiente imagen de ambos pies:</p>
+<p class="centrado"><img alt="" src="./img/plantar-colores-4.jpg" /></p>
+<p>Podemos observar que los dos pies son un poco diferentes, de forma por ejemplo que el bazo se refleja solo en el pie izquierdo al igual que el corazón, mientras que la vesícula biliar se refleja solo en el derecho.</p>
+<p>A partir de aquí lo que hace la reflexología podal es utilizar la técnica del masaje en zonas concretas de los pies para estimular órganos concretos de nuestro cuerpo, estimulando y haciendo circular la energía hacia esos órganos concretos a través de dichos masajes en los pies.</p>
+<h3>Beneficios físicos de la reflexología podal</h3>
+<p>Como veremos, los beneficios de la reflexología podal son múltiples (beneficios que están avalados por múltiples estudios científicos) y podemos dividirlos en lo que incide directamente en el plano físico (mejorando nuestro cuerpo) y lo que incide en el plano mental (mejorando nuestra mente). Es importante destacar que se trata de una terapia natural y de las llamadas terapias alternativas y que por lo tanto, es una terapia complementaria, que, como veremos, ayuda, favorece y mejora, pero que nunca debe cogerse como una terapia curativa por sí sola, ni como sustituta de las terapias médicas recomendadas.</p>
+<p> </p>
+<p><strong>Empecemos pues, con los beneficios que nos encontramos a <u>nivel físico</u>:</strong></p>
+<ul><li>Reduce la tensión muscular, ya que en el fondo estamos hablando de un masaje, por lo tanto, actúa como relajante muscular</li>
+<li>Reduce la sensación de cansancio y fatiga física</li>
+<li>Actúa como favorecedor del sueño, proporcionando un sueño reparador y de mucha más calidad. Es por tanto muy útil en casos de pacientes con insomnio</li>
+<li>Estimula la capacidad de auto curación del propio cuerpo</li>
+<li>Actúa como un estimulante de la circulación, tanto de la sangre como de la linfa</li>
+<li>Estimula las defensas y el sistema inmunológico en general</li>
+<li>Favorece la eliminación de toxinas y sustancias químicas y/o tóxicas de nuestro cuerpo, tales como el café, el tabaco, el alcohol o los medicamentos por ejemplo. Por lo tanto ayuda a mejorar nuestro metabolismo</li>
+<li>Favorece la eliminación de productos de desecho, grasas y azúcares, como consecuencia de la mala alimentación de nuestros días</li>
+<li>Es efectivo para mejorar problemas de tipo gastro intestinal, entre ellos mejora el estreñimiento</li>
+<li>Actúa como un analgésico contra el dolor, siendo muy eficaz y utilizada para mejorar los dolores tales como los menstruales, los dolores de cabeza de tipo migrañoso o también dolores de enfermedades crónicas como la fibromialgia</li>
+<li>Ayuda a fortalecer los músculos, huesos y tendones del pie</li>
+<li>Es una excelente herramienta para la prevención de múltiples patologías físicas</li>
+</ul><p> </p>
+<p><strong>Beneficios <u>mentales y emocionales</u> de la reflexología podal</strong></p>
+<ul><li>Reequilibra todo nuestro sistema energético, ya que con el masaje hacemos fluir y circular toda la energía a través de nuestro cuerpo. Esto hace que también aumente nuestra vitalidad general</li>
+<li>Favorece el equilibrio emocional y mental, aumentando el bienestar y la armonía mental</li>
+<li>Actúa como relajante de la mente</li>
+<li>Disminuye el estrés</li>
+<li>Mejora la concentración</li>
+<li>Nos ayuda a ser más creativos</li>
+<li>Mejora mucho el estado de ánimo y el humor en general</li>
+<li>Al igual que en el apartado anterior, también debemos destacar aquí que es una excelente herramienta de prevención de patologías a nivel mental y emocional, así como patologías de tipo psico somático, ya que como se ha demostrado en múltiples estudios científicos, la ansiedad y el estrés son los causantes de muchas patologías actuales. Con la reflexología podal conseguimos disminuir notablemente los niveles de estrés y ansiedad y por lo tanto, disminuir también muchas patologías asociadas.</li>
+</ul><p>Como puedes ver, la reflexología podal presenta múltiples beneficios sobre el papel, pero en la práctica ¿crees que también es así? ¿Practicas la reflexología podal o te has sometido alguna vez a alguna sesión de reflexología podal? ¿Has experimentado alguno de estos beneficios? ¿Cuáles? ¿Has notado algún otro beneficio que no hayamos comentado?</p>
+</div>
+`;
---
+++ js/main.js
@@ -0,0 +1,168 @@
+//VARIABLES
+const correoValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const telefonoValido = /^(?:(?:00)?549?)?0?(?:11|[2368]\d)(?:(?=\d{0,2}15)\d{2})??\d{8}$/;
+const limpiarFormulario = document.getElementById("form");
+const inputs = document.getElementsByClassName('formularioInput');
+
+//MUESTRA INTRODUCCION
+const contenedor = document.querySelector('.contenedor');
+contenedor.innerHTML = explicacion;
+
+//TOMA LOS SISTEMAS DE REFLEXOLOGIA CON UN JSON LOCAL
+const url = '../js/sistemas.json';
+function mostrarSistemas() {
+  const contentSistemas = document.querySelector('.sistemas');
+  fetch(url)
+    .then(respuesta => respuesta.json())
+    .then(sistemas => {
+      sistemas.forEach(sistema => {
+        if (sistema.id == 1) {
+          contentSistemas.innerHTML +=
+          `<div class="carousel-item active" id=sistema${sistema.id}>
+            <div class="carousel-caption d-none d-md-block">
+              <p style="background-color:rgba(0, 0, 0, 0.5);"><strong>${sistema.texto}</strong></p>
+            </div>
+          <img src=${sistema.imagen} class="d-block w-100 medidasImgH" alt="Reflexología podal">
+          </div>`
+        } else {
+          contentSistemas.innerHTML +=
+          `<div class="carousel-item" id=sistema${sistema.id}>
+            <div class="carousel-caption d-none d-md-block">
+              <p style="background-color:rgba(0, 0, 0, 0.5);"><strong>${sistema.texto}</strong></p>
+            </div>
+          <img src=${sistema.imagen} class="d-block w-100 medidasImgH" alt="Reflexología podal">
+          </div>`
+        }
+      })
+    })
+    .catch(error => console.log(error))
+}
+mostrarSistemas();
+// function mostrarSistemas() {
+//   const contentSistemas = document.querySelector('.sistemas');
+//   fetch(url)
+//     .then(respuesta => respuesta.json())
+//     .then(sistemas => {
+//       sistemas.forEach(sistema => {
+//         contentSistemas.innerHTML += `<p align="center" id=sistema${sistema.id}> <strong>${sistema.texto}</strong><br> 
+//         <img src=${sistema.imagen} class=medidasImgH alt="Reflexología podal"></p>`;
+//       })
+//     })
+//     .catch(error => console.log(error))
+// }
+// mostrarSistemas();
+
+//ARMA FAQS CON UN JSON LOCAL
+function mostrarFaqs() {
+  const urlFaqs = '../js/faq.json';
+  const contentFaqs = document.querySelector('.faq');
+  fetch(urlFaqs).then(respuesta => respuesta.json()).then(faqs => {
+    const contentFaqs = document.querySelector('.faq');
+    for (let item of faqs) {
+      contentFaqs.innerHTML +=
+        `<li class='que'><img class=img src='./img/arrow.png' alt='flecha'>${item.pregunta}</li>
+         <li class='ans'>${item.respuesta}</li>`;
+    }
+  }
+  )
+    .catch(error => console.log(error))
+}
+mostrarFaqs();
+
+//MUESTRA RESPUESTAS ANIMADAS
+const speed = 700;
+document.getElementById('faq').addEventListener('click', function (e) {
+  $(e.path[0]).next().slideToggle(speed).siblings('li.ans').slideUp();
+  const img = $(e.path[0]).children('.img');
+  img.toggleClass('rotate');
+  $('.img').not(img).removeClass('rotate');
+})
+
+//FORMULARIO DE CONTACTO
+for (var i = 0; i < inputs.length; i++) {
+  inputs[i].addEventListener('keyup', function () {
+    if (this.value.length >= 1) {
+      this.nextElementSibling.classList.add('fijar');
+    } else {
+      this.nextElementSibling.classList.remove('fijar');
+    }
+  })
+}
+
+const validateForm = () => {
+  const nombre = document.forms["contacto"]["nombre"]
+  const correo = document.forms["contacto"]["correo"]
+  const telefono = document.forms["contacto"]["telefono"]
+  const mensaje = document.forms["contacto"]["mensaje"]
+
+  if (!nombre.value) {
+    return noValido(nombre, 'El nombre no puede ser vacio')
+  } else if (nombre.value.length < 4) {
+    return noValido(nombre, 'El nombre es muy corto')
+  } else {
+    resultado.style.display = "none";
+    const div = document.getElementById(`div-${nombre.id}`);
+    div.removeChild(div.lastChild)
+    console.log(nombre.value);
+  }
+
+  if (!correo.value) {
+    return noValido(correo, 'El correo no puede estar vacio')
+  } else if (!correoValido.test(correo.value)) {
+    return noValido(correo, 'El correo no es valido')
+  } else {
+    const div = document.getElementById(`div-${correo.id}`);
+    div.removeChild(div.lastChild)
+    console.log(correo.value);
+  }
+
+  if (!telefono.value) {
+    return noValido(telefono, 'El telefono no puede estar vacio')
+  } else if (!telefonoValido.test(telefono.value)) {
+    return noValido(telefono, 'El telefono no es valido')
+  } else {
+    const div = document.getElementById(`div-${telefono.id}`);
+    div.removeChild(div.lastChild)
+    console.log(telefono.value);
+  }
+
+  if (!mensaje.value) {
+    return noValido(mensaje, 'El mensaje no puede ser vacio')
+  } else if (mensaje.value.length < 15) {
+    return noValido(mensaje, 'El mensaje es muy corto')
+  } else {
+    const div = document.getElementById(`div-${mensaje.id}`);
+    div.removeChild(div.lastChild)
+    console.log(mensaje.value);
+  }
+  resultado.innerHTML = "<span>Te suscribiste correctamente </span>";
+  resultado.style.color = "green";
+  resultado.style.backgroundColor = "rgba(122, 245, 147, 0.5)";
+  resultado.style.border = "green solid 2px";
+  resultado.style.display = "block";
+}
+
+const noValido = (campo, mensaje) => {
+  // console.log(campo.id);
+  const div = document.getElementById('resultado');
+  const contenido = document.createTextNode(mensaje);
+  div.appendChild(contenido);
+  resultado.style.display = "block";
+  campo.focus();
+  return false;
+}
+
+document.getElementById('formReset').addEventListener('click', function () {
+  resultado.style.display = "none";
+  nombre.focus();
+})
+
+// const noValido = (campo, mensaje) => {
+//   console.log(campo.id);
+//   const div = document.getElementById(`div-${campo.id}`);
+//   const contenido = document.createTextNode(`${mensaje}`);
+//   div.appendChild(contenido);
+//   campo.focus();
+//   return false;
+// }
+
